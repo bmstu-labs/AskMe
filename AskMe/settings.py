0a11b72cf0f2eb8dfb71665b3ee560e5bc7c7957
johanngorban <ivangorbanwork@gmail.com>
2025-11-12 16:19:36 +0300

    build: localhost allowed, debug turned off

--- AskMe/settings.py
+++ AskMe/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ksniv#f$fsplxw*z*9lja(f21^f28dqc$7+v@v7%9_&(-xn5wu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1'
+]
 
 
 # Application definition

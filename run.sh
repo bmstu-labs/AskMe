1f698c63b70f0f069e41722d1a1a3630d2f20e5e
der-johanni <ivangorbanwork@gmail.com>
2025-11-13 21:10:06 +0300

    build: docker containerization added

--- run.sh
+++ run.sh
@@ -1,3 +1,15 @@
-#!bin/bash
+#!/bin/bash
 
-python manage.py runserver
+set -e
+
+IMAGE_NAME="askme"
+CONTAINER_NAME="askme"
+
+docker build -t $IMAGE_NAME .
+
+docker stop $CONTAINER_NAME 2> /dev/null || true
+docker rm $CONTAINER_NAME 2> /dev/null || true
+
+docker run -d --name $IMAGE_NAME -p 8000:8000 $CONTAINER_NAME
+
+echo "Container started: http://127.0.0.1:8000"
